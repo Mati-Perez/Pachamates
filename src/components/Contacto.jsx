a4b5cd8fd95c4e0b05ecf636fb17a03232629fdc
Matias Perez <matiperez881@gmail.com>
2025-07-17 17:31:53 -0300

    Se arreglo tamaño del button en contactos

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -11,7 +11,7 @@ export default function ContactoSection() {
         <TextField fullWidth label="Nombre" margin="normal" sx={{ backgroundColor: 'white', borderRadius: 1}}/>
         <TextField fullWidth label="Email" type="email" margin="normal" sx={{ backgroundColor: 'white', borderRadius: 1}}/>
         <TextField fullWidth label="Mensaje" multiline rows={4} margin="normal"sx={{ backgroundColor: 'white', borderRadius: 1}} />
-        <Button variant="contained" sx={{ mt: 2, backgroundColor: '#101d20ff','&:hover':{backgroundColor: '#5E7892'}, maxWidth: '400px', width: {xs: '50vh', sm: '30%', md: '30%'}}} fullWidth>
+        <Button variant="contained" sx={{ mt: 2, backgroundColor: '#101d20ff','&:hover':{backgroundColor: '#5E7892'}, maxWidth: '400px', width: {xs: '50%', sm: '50%', md: '50%'}}} fullWidth>
           Enviar
         </Button>
       </Box>
